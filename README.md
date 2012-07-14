678ec17cdef451700067650c2cffd011c916abd9
Sybrand <sybrand@oneiric-desktop.(none)>
2012-07-14 09:11:01 +0200

    getting the framework of the project in place

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-digital-panda
-=============
+# digital-panda
 
 Openstack client
+
+## License
+
+This Software is licensed under the MIT License (MIT), please refer to LICENSE for details
---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name = 'Digital Panda',
+    description = 'Cloud Storage',
+    packages = ['digitalpanda'],
+    author = 'Sybrand Strauss',
+    classifiers = ['Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: X11 Applications',
+        'Intended Audience :: End Users/Desktop',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: Desktop Environment',
+        'Topic :: Internet']
+    )
